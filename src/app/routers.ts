dd8ab022371199765fc4ce92fb1bdb00e22db512
ElHurta <juan.david.hurtado08@gmail.com>
2023-07-24 12:11:32 -0500

    creating first components, cleaning default styles

---
+++ src/app/routers.ts
@@ -0,0 +1,11 @@
+interface Routes {
+    label: string;
+    path: string;
+}
+
+export const routes: Routes[] = [
+    { label: 'Me', path: '/' },
+    { label: 'Skills', path: '/skills' },
+    { label: 'Projects', path: '/projects' },
+    { label: 'Contact', path: '/contact' },
+]

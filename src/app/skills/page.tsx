86d2ec3a3ac92f93aa8a34d1519b25d4bf079254
ElHurta <juan.david.hurtado08@gmail.com>
2023-08-02 20:09:50 -0500

    working on skills section structure

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+'use client'
+import SkillsList from "./components/SkillsList";
+import SkillDetail from "./components/SkillDetail";
 
-export default async function SkillsPage() {
+import { useState } from "react"
+import VerticalBarSVG from '../components/Icons/VerticalBarSVG';
+
+import styles from './page.module.css'
+
+export default function SkillsPage({}) {
+
+  const [selectedSkill, setSelectedSkill] = useState('')
+
   return (
     <>
       <h1 className='section-title'>SKILLS</h1>
+      <article className={styles.skillSection}>
+        <VerticalBarSVG
+          height={200}
+        />
+        <SkillsList />
+        <SkillDetail />
+      </article>
     </>
   )
 }
